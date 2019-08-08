9d5a6e284c294b076c92250977930cbca926cf17
Piotr Bielak <piotrbielak2@gmail.com>
2019-08-08 21:41:37 +0200

    i3gaps to traditional i3 + removed resolutions

--- nix/modules/xserver.nix
+++ nix/modules/xserver.nix
@@ -3,7 +3,6 @@
 
 {
   # Enable the X11 windowing system.
-  # services.xserver.enable = true;
   # services.xserver.layout = "us";
   # services.xserver.xkbOptions = "eurosign:e";
 
@@ -28,15 +27,10 @@
       naturalScrolling = true;
     };
 
-    resolutions = [
-      { x = 1440; y = 900; }
-    ];
-
     windowManager.i3 = {
       enable = true;
-      package = pkgs.i3-gaps;
+      package = pkgs.i3;
       extraPackages = with pkgs; [
-        dmenu
         i3status
         i3lock
         i3blocks
